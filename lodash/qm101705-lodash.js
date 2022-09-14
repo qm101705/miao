9928cabc0859ec85879074c712caaa550edb30d8
qiao <11112@qq.com>
2022-09-14 20:47:06 +0800

    k

--- lodash/qm101705-lodash.js
+++ lodash/qm101705-lodash.js
@@ -6,12 +6,21 @@ var qm101705 = {
       for (var j = 0; j < size; j++) {
         res.push(arry[i])
         i++
-        if (i == size) {
+        if (i == array.length) {
           break
         }
       }
     }
     result.push(res)
+    return result
+  }
+  compact: function (array) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (arry[i]) {
+        result.push(arry[i])
+      }
+    }
     return result
   }
 }
