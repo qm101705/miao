6c67be381d44910f0b04720e821a584acc0d496f
qiao <11112@qq.com>
2022-09-14 20:33:33 +0800

    k

--- lodash/qm101705-lodash.js
+++ lodash/qm101705-lodash.js
@@ -2,16 +2,12 @@ var qm101705 = {
   chuck: function (array, size = 1) {
     var result = []
     var res = []
-    var r = []
     for (var i = 0; i < array.length; i++) {
       if (i < size) {
         res.push(array[i])
-      } else {
-        r.push(array[i])
       }
     }
     result.push(res)
-    result.push(r)
     return result
   }
 }
